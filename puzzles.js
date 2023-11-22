7c116c281bbf61f741d7aca88e1146a5444ad70e
Nicolas <nicolasboulenc@gmail.com>
2023-11-22 17:03:39 +0000

    yet another commit

--- puzzles.js
+++ puzzles.js
@@ -5,7 +5,10 @@ const PUZZLES = [
 		"type":"Introduction",
 		puzzles: [
 			{"rows":3,"cols":3,"elems":[], "grid":[1, 1, 1, 1, 0, 1, 1, 1, 1]},
-			{"rows":3,"cols":3,"elems":[{"x":1,"y":0,"type":"wall","facing":"S"},{"x":2,"y":0,"type":"exit","facing":"N"},{"x":0,"y":2,"type":"exit","facing":"S"}]},
+			{"rows":3,"cols":3,"elems":[
+				{"x":1,"y":0,"type":"wall","facing":"S"},
+				{"x":2,"y":0,"type":"exit","facing":"N"},
+				{"x":0,"y":2,"type":"exit","facing":"S"}	]},
 		]
 	},
 ];
